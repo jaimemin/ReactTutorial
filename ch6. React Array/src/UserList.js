e0a277b1eb637d35a656cce746f649338fcca18b
jaimemin <jaimemin@gmail.com>
2020-04-12 17:17:15 +0900

    simple array

---
+++ ch6. React Array/src/UserList.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+function User({ user }) {
+    return (
+        <div>
+            <b>{user.username}</b> <span>P{user.email}</span>
+        </div>
+    )
+}
+
+function UserList() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+  return (
+    <div>
+      {users.map(user => (
+        <User user={user} key={user.id} />
+      ))}
+    </div>
+    /*
+    <div>
+      {users.map((user, index) => (
+        <User user={user} key={index} />
+      ))}
+    </div>
+    */
+  );
+}
+
+export default UserList;
